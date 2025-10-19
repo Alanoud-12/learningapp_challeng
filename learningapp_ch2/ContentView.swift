04418197406dd40c8e118b58ace4fe9ae445856e
Alanoud-12 <alanoud.alrasheed911@gmail.com>
2025-10-19 16:34:30 +0300

    Initial Commit

---
+++ learningapp_ch2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learningapp_ch2
+//
+//  Created by العنود عبدالله ناصر الرشيد on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learningapp_ch2/learningapp_ch2App.swift
@@ -0,0 +1,17 @@
+//
+//  learningapp_ch2App.swift
+//  learningapp_ch2
+//
+//  Created by العنود عبدالله ناصر الرشيد on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct learningapp_ch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
